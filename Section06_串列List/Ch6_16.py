e9905bb8ebff6213a164ad70b21420298f5040de
Yichen-Hsu <yichenhello@gmail.com>
2022-03-12 01:05:11 +0800

    new update

---
+++ Section06_串列List/Ch6_16.py
@@ -0,0 +1,12 @@
+'''
+串列元素的加法運作
+'''
+James = ['Lebron James', 23, 19, 22, 31, 18] #定義James串列
+ethan = ['Ethan Hsu', 39, 49, 29 ,10 ,30] #定義Ethan串列
+game3 = James[4] + ethan[4]
+Jegmae = James[0] + '和' + ethan[0] + '第四場總得分='
+print(Jegmae, game3)
+
+'''
+刪除串列元素
+'''
