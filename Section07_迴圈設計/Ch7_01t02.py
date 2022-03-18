e6e199a6e9e2a1c2c57ebfca7759086cece1a68f
Yichen-Hsu <yichenhello@gmail.com>
2022-03-18 23:32:45 +0800

    new update

---
+++ Section07_迴圈設計/Ch7_01t02.py
@@ -0,0 +1,19 @@
+# Ch7_01.py 設計1加到10的程式，然後列印結果
+sum = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+print(sum)
+
+# l = []
+# for i in range(11):
+#     l.append(i)
+# print(sum(l))
+
+# Ch7_02.py 一個不完整且不切實際的程式。
+
+vipNames = ['peter', 'roger', 'Kevin', 'Brainda', ... , 'Nadel'] # 等一千筆資料
+print("客戶1 =", vipNames[0])
+print("客戶2 =", vipNames[1])
+print("客戶3 =", vipNames[2])
+
+'''
+碰到以上問題當然不能用一筆一筆方式輸入處理，因此才要使用迴圈方式快速解決
+'''
