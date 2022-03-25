5dc6e6f77d1b60e6b7305a85375dd8910767e05d
Yichen-Hsu <yichenhello@gmail.com>
2022-03-25 21:54:03 +0800

    new update

--- Section06_串列List/Ch6_36t40.py
+++ Section06_串列List/Ch6_36t40.py
@@ -42,3 +42,43 @@ print(f'姓名: {name}')
 print(f'位置: {position}')
 print(f'出生日期: {birth}')
 print(f'在第 {i} 場得分最高 {score_Max}')
+
+'''
+也可以使用append()函數將某一串列插入另一串列的末端
+'''
+
+# Ch6_38.py 使用append()將串列插入另一串列的末端
+cars1 = ['toyota', 'nissan', 'honda']
+cars2 = ['BMW', 'ford']
+print('原先cars1串列內容: ', cars1)
+print('原先cars2串列內容: ', cars2)
+cars1.append(cars2)
+print(f'執行append()後串列cars1內容 = {cars1}')
+
+
+'''
+extend() 會將串列分解成元素，一一插入被extend的串列。
+使用格式如下:
+
+串列A.extend(串列B)
+'''
+
+# Ch6_39.py 
+A = ['Hello', 'World', "How"]
+B = ['Are', 'You']
+A.extend(B)
+print(f'執行extend()之後的A: {A}')
+
+'''
+二為串列(two dimension list)
+'''
+
+# Ch6_40.py 使用二維串列的成績系統總分計算。
+sc = [
+    ['Ethan', 59, 69, 79, 0],
+    ['Cathy', 60, 70, 49, 0]
+]
+sc[0][4] = sum(sc[0][1:4])
+sc[1][4] = sum(sc[1][1:4])
+print(sc[0])
+print(sc[1])
