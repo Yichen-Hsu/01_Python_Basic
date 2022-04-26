86de03db4802ee89fb59e5d87e462f7c9c0f1610
Yichen-Hsu <yichenhello@gmail.com>
2022-04-26 21:17:10 +0800

    new update

--- Section07_迴圈設計/Ch7_29t34.py
+++ Section07_迴圈設計/Ch7_29t34.py
@@ -42,3 +42,11 @@ while n:
     sum += n
     n = int(input("請輸入一個值: "))
 print("輸入總和 = ", sum)
+
+# Ch7_34.py 
+tuition = 50000
+year = 0
+while tuition < 60000:
+    tuition = int(tuition * 1.05)
+    year += 1
+print(f'經過{year}年後學費會達到或超過60000元')
